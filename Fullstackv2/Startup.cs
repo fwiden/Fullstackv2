c74c9a3c97c72b08963809b882d20772e1005d07
Fredrik Widen <fwiden@gmail.com>
2016-09-05 17:17:57 +0200

    Add project files.

---
+++ Fullstackv2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fullstackv2.Startup))]
+namespace Fullstackv2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
